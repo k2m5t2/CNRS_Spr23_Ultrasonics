bcea7d176a734e5d8e1e767e473c921e2fb999cf
CHO <ycho2@msad.georgiatech-metz.fr>
2023-03-29 18:05:24 +0200

    upload more old backups

---
+++ old backups/test.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Evaluating the difference of LW spectra at different distance
+
+@author: dickysilitonga
+"""
+import pandas as pd
+import mat4py
+from mat4py import loadmat
+import scipy
+import numpy as np
+from scipy.fft import fft, ifft, fftfreq
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.colors as colors
+import matplotlib.cbook as cbook
+from matplotlib import cm
+import os
+os.system('reset')
+
+NoOfFiles = 8
+ExpData = {}
+ExpTime = {}
+ExpDist = {}
+
+for i in range(0,NoOfFiles):
+    FileName = 'Si_0215_'+str(i+1)+'.mat'
+    StructName = 'Si_0215_'+str(i+1)
+    ExpData[i] = loadmat(FileName)
+    ExpTime[i] = ExpData[i][StructName]['axisValues'][0]
+    ExpDist[i] = ExpData[i][StructName]['axisValues'][1]
+
+ExpNoYPosPoints = {}
+ExpNoTimePoints = {}
+# ExpSignalValues = {}
+ExpAmpMatrix = {}
+ExpAmpMatrixNorm = {}
+ExpNoXPosPoints = NoOfFiles
+ExpMaxAmpMatrix = np.empty(shape=(NoOfFiles,len(ExpDist[1])))
+                        
+for i in range(0,ExpNoXPosPoints):
+    ExpNoYPosPoints[i] = len(ExpDist[1])
+    ExpNoTimePoints[i] = len(ExpTime[1])
+    ExpAmpMatrix[i] = np.empty(shape=(ExpNoTimePoints[i],ExpNoYPosPoints[i]))
+    ExpAmpMatrixNorm[i] = np.empty(shape=(ExpNoTimePoints[i],ExpNoYPosPoints[i]))
+    for j in range(0,ExpNoYPosPoints[i]):
+        ExpSignalValues = list()
+        for k in range(0,ExpNoTimePoints[i]):
+            ExpSignalValues.append(ExpData[i]['Si_0215_'+str(i+1)]['data'][k][j])
+        ExpAmpMatrix[i][:,j] = ExpSignalValues
+        MaxAmpAbs = max(abs(v) for v in ExpSignalValues)
+        Normalized = [x / MaxAmpAbs for x in ExpSignalValues]
+        ExpAmpMatrixNorm[i][:,j] = Normalized
+        ExpMaxAmpMatrix[i,j] = MaxAmpAbs
+        ExpSignalValues = []
+
+# SAMPLE OF TIME SIGNAL OF A POINT (CHOOSE POINT INDEX 0-159)
+PointToPlot = 2
+plt.plot(ExpTime[PointToPlot],ExpAmpMatrixNorm[PointToPlot][:,PointToPlot],linewidth=0.5,color='r')
+plt.xlim([min(ExpTime[0]),max(ExpTime[0])])
+plt.ylim([-1.2,1.2])
+plt.title("Waveform at a point")
+plt.xlabel("time (us)")
+plt.ylabel("Amplitude (Norm)")
+
+SignalIntDist = ExpMaxAmpMatrix
+xAxis = [0,0.25,0.5,0.75,1.00,1.25,1.50,1.75]
+fig, ax = plt.subplots()
+ax.pcolormesh(ExpDist[0], xAxis, SignalIntDist,
+              vmin=0.01, vmax=0.1, cmap='autumn', shading='auto')
+ax.set_frame_on(False) # remove all spines
+# plt.axis([0, 1.75, 0, max(ExpDist[0])])
+
+
+# ExpFFTSignal = abs(fft(ExpAmplitude[0]))
+
+# plt.plot(ExpFFTSignal)
+
+
+
+# ExpFFTSignal = {}
+# ExpFFTSignalNorm = {}    
+# for i in range(0,ExpNoPosPoints):
+#     ExpFFTSignal[i] = abs(fft(ExpAmplitude[i]))
+#     ExpFFTSignalNorm[i] = ExpFFTSignal[i]/max(ExpFFTSignal[i])
+
+# # Plot the FFT
+# ExpTimeInterval = (ExpTime[1]-ExpTime[0])*1e-6
+# ExpFreqAxis = ((1/ExpTimeInterval)*np.linspace(0,1,ExpNoTimePoints))
+# for i in range(0,ExpNoPosPoints):
+#     plt.plot(ExpFreqAxis,ExpFFTSignalNorm[i],linewidth=0.5,color='r')
+# plt.xlim([0,1.501e6])
+# plt.ylim([0,1])
+# plt.yticks(np.arange(0, 1, 0.1))
+# plt.xticks(np.arange(0, 1.501e6, 0.1e6))
+# plt.title("FFT at each point")
+# plt.xlabel("frequency (Hz)")
+# plt.ylabel("Amplitude (Norm)")
+# plt.show()
+
+# IntensityDistribution = pd.DataFrame.from_dict(ExpFFTSignalNorm, orient='index')
+
+# fig, ax = plt.subplots()
+# ax.pcolormesh(ExpFreqAxis, ExpDist, IntensityDistribution, vmin=0, vmax=0.07)
+# ax.set_frame_on(False) # remove all spines
+# plt.axis([0, 1.5e6, 0, 100])
